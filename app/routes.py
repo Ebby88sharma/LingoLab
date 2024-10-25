5c88648c06ea23da6ab847180a0d8d2d563ce730
Abhinav Sharma <ab88sharma@gmail.com>
2024-10-25 22:40:28 +0000

    languages icon heroku issue testing deployment

--- app/routes.py
+++ app/routes.py
@@ -4,3 +4,7 @@ from app import app
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return redirect(url_for('static', filename='languages.png'))
